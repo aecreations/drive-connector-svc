57b8c6338bb4a5f58cd46f32413fc575fdf62314
AE Creations <aecreations@users.noreply.github.com>
2022-02-20 21:16:54 -0800

    Initial commit

---
+++ src/aeGoogleDrive.py
@@ -0,0 +1,189 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import json
+import requests
+import urllib.parse
+
+
+AUTHZ_SRV_KEY = "googledrive"
+SYNC_FILENAME = "readnext.json"
+SYNC_FILE_MIME_TYPE = "application/json"
+
+_oauthClient = {
+    'accessToken': None,
+    'refreshToken': None,
+    }
+_syncFileID = None
+_isAccessTokenRefreshed = False
+
+
+def init(accessToken, refreshToken, syncFileID):
+    global _oauthClient, _syncFileID    
+    _oauthClient['accessToken'] = accessToken
+    _oauthClient['refreshToken'] = refreshToken
+    _syncFileID = syncFileID
+
+
+def isAccessTokenRefreshed():
+    return _isAccessTokenRefreshed
+
+
+def getAccessToken():
+    return _oauthClient['accessToken']
+
+
+def syncFileExists():
+    global _syncFileID
+    fileExists = False
+    srchFilter = urllib.parse.quote("name='readnext.json'")
+    query = f"q={srchFilter}&spaces=drive&trashed=false"
+    reqOpts = {
+        'method': "GET",
+        'headers': _getReqHdrs(),
+        }
+
+    # BUG!! Trashed files incorrectly returned even if `trashed=false` was set
+    resp = _fetch(f"https://www.googleapis.com/drive/v3/files?{query}", reqOpts)
+    resp.raise_for_status()
+    respBody = resp.json()
+    files = respBody['files']
+    if len(files) > 0:
+        filteredFiles = filter(lambda file:
+                                   file['name'] == SYNC_FILENAME
+                                   and file['kind'] == "drive#file"
+                                   and file['mimeType'] == SYNC_FILE_MIME_TYPE,
+                          files)
+        fileList = list(filteredFiles)
+        if len(fileList) > 0:
+            _syncFileID = fileList[0]['id']
+            fileExists = True
+    return (fileExists, _syncFileID)
+
+
+def createSyncFile(jsonStr):
+    syncFileID, fileCreatedTime = _setSyncData(jsonStr, True)   
+    return (syncFileID, fileCreatedTime)
+
+
+def getSyncData():
+    if _syncFileID is None:
+        raise ValueError
+    query = "alt=media"
+    headers = _getReqHdrs();
+    reqOpts = {
+        'method': "GET",
+        'headers': headers,
+        }
+    resp = _fetch(f"https://www.googleapis.com/drive/v3/files/{_syncFileID}?{query}", reqOpts)
+    resp.raise_for_status()
+    syncData = resp.text
+    return syncData
+
+
+def setSyncData(jsonStr):
+    syncFileID, fileModifiedTime = _setSyncData(jsonStr, False)
+    return (syncFileID, fileModifiedTime)
+
+
+def getLastModifiedTime():
+    if _syncFileID is None:
+        raise ValueError
+    query = "fields=modifiedTime"
+    headers = _getReqHdrs();
+    reqOpts = {
+        'method': "GET",
+        'headers': headers,
+        }
+    resp = _fetch(f"https://www.googleapis.com/drive/v3/files/{_syncFileID}?{query}", reqOpts)
+    resp.raise_for_status()
+    respBody = resp.json()
+    lastModifiedTime = respBody['modifiedTime']
+    return lastModifiedTime
+    
+
+#
+# Helpers
+#
+
+def _setSyncData(syncData, isNewFile):
+    global _syncFileID
+    query = "uploadType=multipart&fields=id,modifiedTime"
+    headers = _getReqHdrs();
+    headers['Content-Type'] = "multipart/related; boundary=ae-boundary"
+    body = '--ae-boundary\n' + \
+      'Content-Type: application/json; charset=UTF-8\n\n' + \
+      '{\n' + \
+      '  "name": "' + SYNC_FILENAME + '",\n' + \
+      '  "kind": "drive#file",\n' + \
+      '  "mimeType": "' + SYNC_FILE_MIME_TYPE + '"\n' + \
+      '}\n' + \
+      '--ae-boundary\n' + \
+      'Content-Type: ' + SYNC_FILE_MIME_TYPE + '\n\n' + \
+      syncData + '\n' + \
+      '--ae-boundary--'
+    headers['Content-Length'] = str(_getLengthInBytes(body))
+    reqOpts = {
+        'method': "POST" if isNewFile else "PATCH",
+        'headers': headers,
+        'body': body
+        }
+    fileIDSfx = "" if isNewFile else f"/{_syncFileID}"
+    resp = _fetch(f"https://www.googleapis.com/upload/drive/v3/files{fileIDSfx}?{query}", reqOpts)
+    resp.raise_for_status()
+    respBody = resp.json()
+    fileID = respBody['id']
+    fileModifiedTime = respBody['modifiedTime']
+    if isNewFile:
+        _syncFileID = fileID
+    return (fileID, fileModifiedTime)
+
+
+def _getLengthInBytes(str):
+    return len(str.encode("utf-8"))
+
+
+def _getReqHdrs():
+    reqHdrs = {
+        'Authorization': f"Bearer {_oauthClient['accessToken']}"
+        }
+    return reqHdrs
+
+
+def _fetch(url, requestOpts, isRetry=False):
+    global _isAccessTokenRefreshed
+    rv = None
+    method = requestOpts['method']
+    args = {}
+    if 'headers' in requestOpts:
+        args['headers'] = requestOpts['headers']
+    if 'body' in requestOpts:
+        args['data'] = requestOpts['body']
+    resp = requests.request(method, url, **args)
+    if resp.status_code == requests.codes.unauthorized:
+        if isRetry is True:
+            # Prevent infinite recursion and just return the error response.
+            rv = resp
+        else:
+            newAccessToken = _getNewAccessToken()
+            _oauthClient['accessToken'] = newAccessToken
+            _isAccessTokenRefreshed = True
+            requestOpts['headers']['Authorization'] = f"Bearer {newAccessToken}"
+            rv = _fetch(url, requestOpts, True)
+    else:
+        rv = resp
+    return rv
+
+    
+def _getNewAccessToken():
+    params = {
+        'svc': AUTHZ_SRV_KEY,
+        'grant_type': "refresh_token",
+        'refresh_token': _oauthClient['refreshToken']
+        }
+    resp = requests.post("https://aeoaps.herokuapp.com/readnext/token", data=params)
+    resp.raise_for_status()   
+    respBody = resp.json()
+    newAccessToken = respBody['access_token']
+    return newAccessToken
---
+++ src/driveConnectorSvc.py
@@ -0,0 +1,115 @@
+#!/usr/local/bin/python3
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import sys
+import struct
+import json
+import aeGoogleDrive
+
+
+DEBUG = True
+
+APP_NAME = "Drive Connector Service"
+APP_VER = "1.0a0+"
+
+
+def getAppName():
+    return APP_NAME
+
+def getAppVer():
+    return APP_VER
+
+def log(msg):
+    if DEBUG:
+        with open("debug.txt", "a") as file:
+            file.write(msg)
+            file.write("\n")
+
+def getMessage():
+    rawLength = sys.stdin.buffer.read(4)
+    if len(rawLength) == 0:
+        sys.exit(0)
+    stdioMsgLen = struct.unpack('@I', rawLength)[0]
+    stdioMsg = sys.stdin.buffer.read(stdioMsgLen).decode('utf-8')
+    message = json.loads(stdioMsg)
+    return message
+
+def encodeMessage(msgData):
+    encodedContent = json.dumps(msgData).encode('utf-8')
+    encodedLength = struct.pack('@I', len(encodedContent))
+    return {'length': encodedLength, 'content': encodedContent}
+
+def sendMessage(encodedMsg):
+    sys.stdout.buffer.write(encodedMsg['length'])
+    sys.stdout.buffer.write(encodedMsg['content'])
+    sys.stdout.buffer.flush()
+
+
+while True:
+    msg = getMessage()
+    resp = None
+    if 'id' not in msg:
+        err = "Error: Missing message ID"
+        log(err)
+        sys.stderr.buffer.write("driveConnectorSvc: " + err)
+        sys.stderr.buffer.flush()
+        sys.exit(1)
+    log('driveConnectorSvc: Received native message "{}"'.format(msg['id']))
+    if msg['id'] == "get-app-version":
+        # accessToken, refreshToken and syncFileID not required.
+        resp = {
+            'appName': getAppName(),
+            'appVersion': getAppVer()
+            }
+        sendMessage(encodeMessage(resp))
+        continue
+    if ('accessToken' not in msg) or ('refreshToken' not in msg) or ('syncFileID' not in msg):
+        err = "Error: Missing accessToken, refreshToken and/or syncFileID"
+        log(err)
+        sys.stderr.buffer.write("driveConnectorSvc: " + err)
+        sys.stderr.buffer.flush()
+        sys.exit(1)
+    initArgs = (
+        msg['accessToken'],
+        msg['refreshToken'],
+        msg['syncFileID']
+        )
+    aeGoogleDrive.init(*initArgs)
+    if msg['id'] == "sync-file-exists":
+        fileExists, syncFileID = aeGoogleDrive.syncFileExists()
+        resp = {
+            'syncFileExists': fileExists,
+            'syncFileID': syncFileID,
+            }
+    elif msg['id'] == "create-sync-file":
+        syncData = json.dumps(msg['syncData'])
+        syncFileID, fileCreatedTime = aeGoogleDrive.createSyncFile(syncData)
+        resp = {
+            'syncFileID': syncFileID,
+            'fileCreatedTime': fileCreatedTime,
+            }
+    elif msg['id'] == "get-sync-data":
+        # TO DO: Break up data into chunks, 25 bookmarks at a time.
+        remoteSyncData = aeGoogleDrive.getSyncData()
+        resp = {
+            'syncData': json.loads(remoteSyncData),
+            'hasMoreItems': False,
+            }
+    elif msg['id'] == "set-sync-data":
+        syncData = json.dumps(msg['syncData'])
+        _, fileModifiedTime = aeGoogleDrive.setSyncData(syncData)
+        resp = {
+            'fileModifiedTime': fileModifiedTime
+            }
+    elif msg['id'] == "get-last-modified-time":        
+        resp = {
+            'lastModifiedTime': aeGoogleDrive.getLastModifiedTime()
+            }
+
+    if aeGoogleDrive.isAccessTokenRefreshed():
+        resp['newAccessToken'] = aeGoogleDrive.getAccessToken()
+
+    if resp is not None:
+        sendMessage(encodeMessage(resp))
